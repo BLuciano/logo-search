147f51b1260d1d7c416c19f9fc27dd22afaeccdc
Luciano Bruzzoni <lucianobruzzoni@yahoo.com>
2016-04-17 17:04:10 -0700

    Mixed the objects array to not look repetitive

--- js/app.js
+++ js/app.js
@@ -10,63 +10,63 @@ function getLogos(){
         school : null
       },
       {
+        gray : 'imgs/acm.png',
+        color : 'imgs/acm-color.png',
+        program : null,
+        school : 'high-school'
+      },
+      {
+        gray : 'imgs/AFE.png',
+        color : 'imgs/AFE-color.png',
+        program : null,
+        school : 'high-school'
+      },
+      {
+        gray : 'imgs/acm.png',
+        color : 'imgs/acm-color.png',
+        program : 'scholarship',
+        school : 'high-school'
+      },
+      {
         gray : 'imgs/aci.png',
         color : 'imgs/aci-color.png',
         program : 'scholarship',
         school : null
       },
       {
+        gray : 'imgs/AFE.png',
+        color : 'imgs/AFE-color.png',
+        program : 'loan',
+        school : 'k-12'
+      },
+      {
         gray : 'imgs/aci.png',
         color : 'imgs/aci-color.png',
         program : 'loan',
         school : 'high-school'
       },
       {
+        gray : 'imgs/acm.png',
+        color : 'imgs/acm-color.png',
+        program : 'scholarship',
+        school : 'high-school'
+      },
+      {
         gray : 'imgs/aci.png',
         color : 'imgs/aci-color.png',
         program : null,
         school : 'k-12'
       },
       {
-        gray : 'imgs/acm.png',
-        color : 'imgs/acm-color.png',
-        program : null,
-        school : 'high-school'
-      },
-      {
-        gray : 'imgs/acm.png',
-        color : 'imgs/acm-color.png',
-        program : 'scholarship',
-        school : 'high-school'
-      },
-      {
-        gray : 'imgs/acm.png',
-        color : 'imgs/acm-color.png',
-        program : 'scholarship',
-        school : 'high-school'
-      },
-      {
-        gray : 'imgs/acm.png',
-        color : 'imgs/acm-color.png',
-        program : 'scholarship',
-        school : 'k-12'
-      },
-      {
         gray : 'imgs/AFE.png',
         color : 'imgs/AFE-color.png',
         program : 'loan',
         school : 'k-12'
       },
       {
-        gray : 'imgs/AFE.png',
-        color : 'imgs/AFE-color.png',
-        program : null,
-        school : 'high-school'
-      },
-      {
-        gray : 'imgs/AFE.png',
-        color : 'imgs/AFE-color.png',
-        program : 'loan',
+        gray : 'imgs/acm.png',
+        color : 'imgs/acm-color.png',
+        program : 'scholarship',
         school : 'k-12'
       },
       {
